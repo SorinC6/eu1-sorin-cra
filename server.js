ae6aabb50531c7ab87cd14667f9d8d368f8204d5
Sorin Chis <kish.sorin@yahoo.com>
2019-03-21 13:04:37 +0200

    commit

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 const express = require('express');
-
 const path = require('path');
 const app = express();
 
-const pathToIndexHtml = path.join(__dirname, 'build', 'index.html');
+// cra works is, when webpack runs, it creates a build folder
+// which contains the index.html and the bundle.js
+
 const pathToBuildFolder = path.join(__dirname, 'build');
+const pathToIndexHtml = path.join(pathToBuildFolder, 'index.html');
 
 console.log(__dirname);
 console.log(pathToIndexHtml);
 console.log(pathToBuildFolder);
 
-//allow static assets to be served off of this folder
+// 2- allow static assets to be served off of this folder (build)!!
 app.use(express.static(pathToBuildFolder));
 
 app.get('/', (req, res) => {
-	res.sendfile(pathToIndexHtml);
+	// 1- res.sendFile
+	res.sendFile(pathToIndexHtml);
 });
 
 app.listen(process.env.PORT || 5000);
